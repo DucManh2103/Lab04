511c2cf9bda4c8e7cabd542719cbe512e3df3f42
DucManh2103 <124852371+DucManh2103@users.noreply.github.com>
2023-05-16 08:55:53 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,26 @@
+package Manh;
+
+import java.util.Scanner;
+
+public class main {
+	public static void main(String[]arg) {
+		List l = new List();
+		int n;
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("nhan 1:Nhap thong tin ");
+			System.out.println("nhan 2:xuat thong tin ");
+			
+	n =sc.nextInt();
+			switch (n)
+			{
+			case 1:l.nhapdl();break;
+			case 2:l.xuat();break;
+			
+			}
+		    }
+		while(true);
+	}
+}
+	
+
